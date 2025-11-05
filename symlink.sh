669c326b79ac7c1c8fe47f807be6c676acd390f5
Pedro Brito <tablet.pfb@gmail.com>
2025-11-05 20:35:39 -0300

    unfinished changes

--- symlink.sh
+++ symlink.sh
@@ -1,28 +1,50 @@
 #---------------------------
 # Variables
-# --------------------------
+#--------------------------
 
 nvimdir="$HOME/.config/nvim"
 tmuxdir="$HOME/.tmux.conf"
 
 #---------------------------
-# Creating the symlink
-# --------------------------
+# Functions
+#--------------------------
+what_to_do() {
 
-if [ ! -d $nvimdir ]; then
-    ln -s "$(pwd)/nvim" "$nvimdir"
-else
     printf "There is already a Neovim config file, what do you want to do?\n\n"
     printf "0.Keep current\n"
     printf "1.Replace (the current configs will be moved elsewhere)\n"
     printf "2.Erase and replace (this will erase the current configs permanently!!)\n\n"
+
+    option=10
+
+    while [ $option -ne 0 ] && [ $option -ne 1 ] && [ $option -ne 2 ]; do
+
+        read option
+
+        case $option in
+        0) echo "ok then" ;;
+        1) echo "replacing" ;;
+        2) echo "erasing and replacing" ;;
+        *) echo "invalid option, seletc 0, 1, or 2:\n" ;;
+        esac
+    done
+    printf "\n"
+}
+
+#---------------------------------------
+# Creating the symlink
+#---------------------------------------
+
+#-------------- Neovim -----------------
+if [ ! -d $nvimdir ]; then
+    ln -s "$(pwd)/nvim" "$nvimdir"
+else
+    what_to_do
 fi
 
+#------------ Tmux --------------------
 if [ ! -d $nvimdir ]; then
     ln -s "$(pwd)/.tmux.conf" "$tmuxdir"
 else
-    printf "There is already a Tmux config file, what do you want to do?\n\n"
-    printf "0.Keep current\n"
-    printf "1.Replace (the current configs will be moved elsewhere)\n"
-    printf "2.Erase and replace (this will erase the current configs permanently!!)\n\n"
+    what_to_do
 fi
