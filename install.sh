a7726cc300f867f8dc991aec555d13daac36a317
pedrofb7 <tablet.pfb@gmail.com>
2026-05-03 12:59:04 -0300

    Nodejs package added and better package identification

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-
 PACKAGES=(
     "nvim"
     "tmux"
     "yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick"
     "gum"
+    "nodejs"
 )
 
 # Installing the packages
 for item in "${PACKAGES[@]}"; do
-	#gets the first name of the package 
-    cmd=$(echo $item | awk '{print $1}')
+    #gets the first name of the package
+    pack=$(echo $item | awk '{print $1}')
 
-    if ! command -v "$cmd" >/dev/null 2>&1; then
-        echo "Installing $cmd..."
-        
+    if ! sudo pacman -Q "$pack" >/dev/null 2>&1; then
+        echo "Installing $pack..."
+
         if sudo pacman -S --noconfirm $item >/dev/null 2>&1; then
-            echo "$cmd ---installed successfully!"
+            echo "$pack ---installed successfully!"
         else
-            echo "Error: Failed to install $cmd."
+            echo "Error: Failed to install $pack."
         fi
     else
-        echo "$cmd ---already installed"
+        echo "$pack ---already installed"
     fi
 done
