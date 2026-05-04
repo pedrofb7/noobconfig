f94ed077a4e01f17215b374f59e19a1b4ee31288
pedrofb7 <tablet.pfb@gmail.com>
2026-05-03 23:45:53 -0300

    Adding more language servers

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,1 +1,17 @@
-vim.lsp.enable("lua_ls");
+--enable function --
+local function Enable(language_server) 
+
+        vim.lsp.enable(language_server)
+end
+
+-- ======== Language servers ========= --
+
+--to load new language servers dowload the server with Mason and enable it here
+--with the same name provided by the nevim-lspconfig repository
+Enable("clangd");
+Enable("cmake");
+Enable("cssls");
+Enable("java_language_server");
+Enable("lua_ls");
+Enable("pyright");
+
