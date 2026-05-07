8e0286bfe8af86f7b239ab1d19bf4cc4562e452b
pedrofb7 <tablet.pfb@gmail.com>
2026-05-07 00:35:54 -0300

    Adding Indent-blankline for visual indenting

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -46,7 +46,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end, opts) -- jump to next diagnostic in buffer
 
     opts.desc = "Show documentation for what is under cursor"
-    keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+    keymap.set("n", "<leader>dc", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
     opts.desc = "Restart LSP"
     keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
