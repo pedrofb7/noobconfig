e525a91e43cf31c6d38a9baf2e35c0c32cd20b7f
pedrofb7 <tablet.pfb@gmail.com>
2026-05-07 16:00:18 -0300

    Copy to the system's clipboard fixed and better description

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -60,16 +60,16 @@ return {
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Search help' })
+    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Search keymaps' })
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search files' })
+    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'Search select telescope' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Search current word' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Search by grep' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Search diagnostics' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Search resume' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Search recent files ("." for repeat)' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
@@ -78,7 +78,7 @@ return {
         winblend = 10,
         previewer = false,
       })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    end, { desc = 'Fuzzily search in current buffer' })
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
@@ -87,6 +87,6 @@ return {
         grep_open_files = true,
         prompt_title = 'Live Grep in Open Files',
       }
-    end, { desc = '[S]earch [/] in Open Files' })
+    end, { desc = 'Search in Open Files' })
   end,
 }
--- .config/nvim/lua/start/remap.lua
+++ .config/nvim/lua/start/remap.lua
@@ -23,81 +23,80 @@ map('n', 'Q', '<Nop>', ' ')
 --- =============== -- Remapping -- ================= ---
 
 --Line navigation--
-map('n', '<leader>4', '$', 'go to the end of the line')
-map('n', '<leader>-', '_', 'go to the first character of the line')
+map('n', '<leader>4', '$', 'Go to the end of the line')
+map('n', '<leader>-', '_', 'Go to the first character of the line')
 
 -- Save and quit --
-map('n', '<C-S>', ':w<CR>', 'save file')
-map('i', '<C-S>', '<ESC>:w<CR>i', 'save file')
-map('n', '<C-q>', '<cmd> q <CR>', 'quit file')
+map('n', '<C-S>', ':w<CR>', 'Save file')
+map('i', '<C-S>', '<ESC>:w<CR>i', 'Save file')
+map('n', '<C-q>', '<cmd> q <CR>', 'Quit file')
 
 -- Movement --
-map('v', 'K', ":m '<-2<CR>gv=gv", 'moving selection up')
-map('v', 'J', ":m '>+1<CR>gv=gv", 'moving selection down')
+map('v', 'K', ":m '<-2<CR>gv=gv", 'Moving selection up')
+map('v', 'J', ":m '>+1<CR>gv=gv", 'Moving selection down')
 
 -- The greatest shortcut of all time --
-map('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], 'serch current word and replace')
+map('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], 'Serch current word and replace')
 
 -- Vertical scroll and center --
-map('n', '<C-d>', '<C-d>zz', 'scroll half screen down')
-map('n', '<C-u>', '<C-u>zz', 'scroll half screen up')
+map('n', '<C-d>', '<C-d>zz', 'Scroll half screen down')
+map('n', '<C-u>', '<C-u>zz', 'Scroll half screen up')
 
 -- Find and center --
-map('n', 'n', 'nzzzv', 'next occurrence of the search')
-map('n', 'N', 'Nzzzv', 'previous occurrence of the search')
+map('n', 'n', 'nzzzv', 'Next occurrence of the search')
+map('n', 'N', 'Nzzzv', 'Previous occurrence of the search')
 
 -- Resize with arrows --
-map('n', '<Up>', ':resize -2<CR>', 'resize split windw up')
-map('n', '<Down>', ':resize +2<CR>', 'resize split windw down')
-map('n', '<Left>', ':vertical resize -2<CR>', 'resize split windw left')
-map('n', '<Right>', ':vertical resize +2<CR>', 'resize split windw right')
+map('n', '<Up>', ':resize -2<CR>', 'Resize split windw up')
+map('n', '<Down>', ':resize +2<CR>', 'Resize split windw down')
+map('n', '<Left>', ':vertical resize -2<CR>', 'Resize split windw left')
+map('n', '<Right>', ':vertical resize +2<CR>', 'Resize split windw right')
 
 -- Buffers --
-map('n', '<Tab>', ':bnext<CR>', 'next buffer')
-map('n', '<S-Tab>', ':bprevious<CR>', 'previous buffer')
-map('n', '<leader>x', ':bdelete!<CR>', 'close buffer')
-map('n', '<leader>b', '<cmd> enew <CR>', 'new buffer')
+map('n', '<Tab>', ':bnext<CR>', 'Next buffer')
+map('n', '<S-Tab>', ':bprevious<CR>', 'Previous buffer')
+map('n', '<leader>x', ':bdelete!<CR>', 'Close buffer')
+map('n', '<leader>b', '<cmd> enew <CR>', 'New buffer')
 
 -- Window management --
-map('n', '<leader>v', '<C-w>v', 'split window vertically')
-map('n', '<leader>h', '<C-w>s', 'split window horrizontally')
-map('n', '<leader>se', '<C-w>=', 'make split window equal')
+map('n', '<leader>v', '<C-w>v', 'Split window vertically')
+map('n', '<leader>h', '<C-w>s', 'Split window horrizontally')
+map('n', '<leader>se', '<C-w>=', 'Make split window equal')
 map('n', '<leader>xs', ':close<CR>', 'close current split window')
 
 -- Navigate between splits --
-map('n', '<C-k>', ':wincmd k<CR>', 'go to up window')
-map('n', '<C-j>', ':wincmd j<CR>', 'go to down window')
-map('n', '<C-h>', ':wincmd h<CR>', 'go to left window')
-map('n', '<C-l>', ':wincmd l<CR>', 'go to right window')
+map('n', '<C-k>', ':wincmd k<CR>', 'Go to up window')
+map('n', '<C-j>', ':wincmd j<CR>', 'Go to down window')
+map('n', '<C-h>', ':wincmd h<CR>', 'Go to left window')
+map('n', '<C-l>', ':wincmd l<CR>', 'Go to right window')
 
 -- Tabs --
-map('n', 'tn', ':tabnew<CR>', 'new tab')
-map('n', 'tq', ':tabclose<CR>', 'close tab')
-map('n', 'tk', ':tabn<CR>', 'next tab')
-map('n', 'tj', ':tabp<CR>', 'previous tab')
+map('n', 'tn', ':tabnew<CR>', 'New tab')
+map('n', 'tq', ':tabclose<CR>', 'Close tab')
+map('n', 'tk', ':tabn<CR>', 'Next tab')
+map('n', 'tj', ':tabp<CR>', 'Previous tab')
 
 -- Toggle line wrapping --
-map('n', '<leader>lw', '<cmd>set wrap!<CR>', 'toggle line wrapping')
+map('n', '<leader>lw', '<cmd>set wrap!<CR>', 'Toggle line wrapping')
 
 -- Stay in indent mode --
-map('v', '<', '<gv', 'indent left')
-map('v', '>', '>gv', 'indent right')
+map('v', '<', '<gv', 'Indent left')
+map('v', '>', '>gv', 'Indent right')
 
 -- Keep last yanked when pasting --
 map('v', 'p', '"_dP', 'paste')
 
 -- Copying the system's clipboard --
-map({ 'n', 'v' }, '<leader>y', [["+y]], "copy to the system's clipboard")
-map('n', '<leader>Y', [["+Y]], "copy to the system's clipboard")
+map({ 'n', 'v' }, '<leader>Y', '"+y', "Copy to the system's clipboard")
 
 --Selection --
-map('n', '<leader>w', 'viw', 'select current word')
+map('n', '<leader>w', 'viw', 'Select current word')
 
 -- Git --
-map('n', '<leader>gs', ':Git status<CR>', 'git status')
-map('n', '<leader>ga', ':Git add', 'git add')
-map('n', '<leader>ga.', ':Git add .<CR>', 'git add .')
-map('n', '<leader>gc', ':Git commit -m ', 'git commit')
-map('n', '<leader>gp', ':Git push<CR> ', 'git push')
-map('n', '<leader>gpl', ':Git pull<CR> ', 'git pull')
-map('n', '<leader>gl', ':Git log<CR> ', 'git log')
+map('n', '<leader>gs', ':Git status<CR>', 'Git status')
+map('n', '<leader>ga', ':Git add', 'Git add')
+map('n', '<leader>ga.', ':Git add .<CR>', 'Git add .')
+map('n', '<leader>gc', ':Git commit -m ', 'Git commit')
+map('n', '<leader>gp', ':Git push<CR> ', 'Git push')
+map('n', '<leader>gpl', ':Git pull<CR> ', 'Git pull')
+map('n', '<leader>gl', ':Git log<CR> ', 'Git log')
