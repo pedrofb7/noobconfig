7860f1f817d6e046d47003db4d970f3aa0c856a7
pedrofb7 <tablet.pfb@gmail.com>
2026-05-10 02:27:56 -0300

    Alpha plugin small correction

--- .config/nvim/lua/config/plugins/alpha.lua
+++ .config/nvim/lua/config/plugins/alpha.lua
@@ -2,14 +2,10 @@ return {
   'goolord/alpha-nvim',
   event = 'VimEnter',
   dependencies = {
-    'nhattVim/alpha-ascii.nvim', -- for ascii headers
+    'nhattVim/alpha-ascii.nvim',
     opts = {
-      -- Header name, use "random" for a random header
       header = 'random',
-
-      -- Use some default headers
       use_default = false,
-
       user_path = vim.fn.stdpath 'config' .. '/ascii',
     },
   },
@@ -17,17 +13,16 @@ return {
     local alpha = require 'alpha'
     local dashboard = require 'alpha.themes.dashboard'
 
+    -- Buttons
     dashboard.section.buttons.val = {
       dashboard.button('SPC f f', '  Find File  ', ':Telescope find_files<CR>'),
       dashboard.button('SPC f o', '  Recent File  ', ':Telescope oldfiles<CR>'),
       dashboard.button('SPC f w', '  Find Word  ', ':Telescope live_grep theme=ivy<CR>'),
       dashboard.button('SPC f b', '  Bookmarks  ', ':Telescope marks theme=ivy<CR>'),
-      -- dashboard.button('SPC f t', '  Themes  ', ':Telescope colorscheme enable_preview=false<CR>'), -- no need
-      -- dashboard.button('SPC f s', '  Settings', ':e $MYVIMRC | :cd %:p:h <CR>'), -- no need
       dashboard.button('SPC c i', '  Change header image', ':AlphaAsciiNext<CR>'),
     }
 
-    -- Display the amount of loaded plugins
+    -- To show plugin stats
     vim.api.nvim_create_autocmd('User', {
       once = true,
       pattern = 'LazyVimStarted',
@@ -47,7 +42,15 @@ return {
       pattern = 'alpha',
       callback = function()
         vim.opt_local.laststatus = 0
-        vim.opt_local.showtabline = 0
+
+        -- To reset the status line
+        vim.api.nvim_create_autocmd('BufUnload', {
+          buffer = 0,
+          once = true,
+          callback = function()
+            vim.opt.laststatus = 3
+          end,
+        })
       end,
     })
 
